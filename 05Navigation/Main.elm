9047e24448c917b9a48cc328845822617392a70a
Simon Winkler <simon.winkler@viu.ch>
2019-01-27 17:23:07 +0100

    Add basic url parsing, show different views depending on the url

--- 05Navigation/Main.elm
+++ 05Navigation/Main.elm
@@ -5,6 +5,7 @@ import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Url
+import Url.Parser exposing ((</>), Parser, int, map, oneOf, s, string, top)
 
 
 
@@ -24,18 +25,44 @@ main =
 
 
 
+-- Routing
+
+
+type Route
+    = LeaderBoard
+    | AddRunner
+    | Login
+    | NotFound
+
+
+routeParser : Parser (Route -> a) a
+routeParser =
+    oneOf
+        [ map LeaderBoard top
+        , map AddRunner (s "add")
+        , map Login (s "login")
+        ]
+
+
+fromUrl : Url.Url -> Route
+fromUrl url =
+    Maybe.withDefault NotFound (Url.Parser.parse routeParser url)
+
+
+
 -- MODEL
 
 
 type alias Model =
     { key : Nav.Key
     , url : Url.Url
+    , page : Route
     }
 
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-    ( Model key url, Cmd.none )
+    ( Model key url LeaderBoard, Cmd.none )
 
 
 
@@ -59,7 +86,7 @@ update msg model =
                     ( model, Nav.load href )
 
         UrlChanged url ->
-            ( { model | url = url }
+            ( { model | url = url, page = fromUrl url }
             , Cmd.none
             )
 
@@ -77,19 +104,46 @@ subscriptions _ =
 -- VIEW
 
 
+viewPage : String -> Html Msg
+viewPage pageDescription =
+    div []
+        [ h3 [] [ text pageDescription ]
+        , p [] [ text <| "TODO: make " ++ pageDescription ]
+        ]
+
+
+menu : Model -> Html Msg
+menu model =
+    ul []
+        [ viewLink "/"
+        , viewLink "/add"
+        , viewLink "/login"
+        ]
+
+
 view : Model -> Browser.Document Msg
 view model =
+    let
+        page =
+            case model.page of
+                LeaderBoard ->
+                    viewPage "LeaderBoard"
+
+                AddRunner ->
+                    viewPage "AddRunner"
+
+                Login ->
+                    viewPage "Login"
+
+                NotFound ->
+                    viewPage "404"
+    in
     { title = "URL Interceptor"
     , body =
         [ text "The current URL is: "
         , b [] [ text (Url.toString model.url) ]
-        , ul []
-            [ viewLink "/home"
-            , viewLink "/profile"
-            , viewLink "/reviews/the-century-of-the-self"
-            , viewLink "/reviews/public-opinion"
-            , viewLink "/reviews/shah-of-shahs"
-            ]
+        , menu model
+        , page
         ]
     }
 
